ca99adf732bdc3dea80f12f1002d114c82e1e93f
T. Isaac Lightburn <isaac@hozed.net>
2014-02-26 04:46:51 -0600

    Forgot to add GPL header to new files Also added a combobox to the regulars dialog to select rotation insertion point (fair, bottom, next)

--- KaraokeHost/regularsingersdialog.cpp
+++ KaraokeHost/regularsingersdialog.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2013-2014 Thomas Isaac Lightburn
+ *
+ *
+ * This file is part of OpenKJ.
+ *
+ * OpenKJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "regularsingersdialog.h"
 #include "ui_regularsingersdialog.h"
 
@@ -14,6 +34,12 @@ RegularSingersDialog::RegularSingersDialog(QWidget *parent) :
     ui->treeViewRegulars->setColumnWidth(5,20);
     ui->treeViewRegulars->header()->setSectionResizeMode(2,QHeaderView::ResizeToContents);
     ui->treeViewRegulars->header()->setSectionResizeMode(1,QHeaderView::Stretch);
+    ui->treeViewRegulars->header()->setSectionResizeMode(3,QHeaderView::Fixed);
+    ui->treeViewRegulars->header()->setSectionResizeMode(4,QHeaderView::Fixed);
+    ui->treeViewRegulars->header()->setSectionResizeMode(5,QHeaderView::Fixed);
+    ui->comboBoxAddPos->addItem("Fair");
+    ui->comboBoxAddPos->addItem("Bottom");
+    ui->comboBoxAddPos->addItem("Next");
 }
 
 RegularSingersDialog::~RegularSingersDialog()
