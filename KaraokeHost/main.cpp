4aff6bfde1c19ce660e5453179ac70c46573c43c
Isaac Lightburn <isaac@hozed.net>
2017-01-25 23:34:03 -0600

    Changing UI style from native to fusion Better UI uniformity across platforms.

--- KaraokeHost/main.cpp
+++ KaraokeHost/main.cpp
@@ -30,7 +30,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
     return a.exec();
