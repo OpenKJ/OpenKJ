b96c5b098a7b87ead837a8ce1e90f1bf6489167f
Isaac Lightburn <isaac@openkj.org>
2018-04-12 17:28:06 -0500

    Use all lowercase in branch name

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 3
-#define OKJ_VERSION_BUILD 1
-#define OKJ_VERSION_STRING "1.3.0"
-#define OKJ_VERSION_BRANCH "Unstable"
+#define OKJ_VERSION_BUILD 2
+#define OKJ_VERSION_STRING "1.3.2"
+#define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
