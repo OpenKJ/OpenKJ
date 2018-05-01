5912bdb6a0dfb8e068feff111b5cb51cae6cda4b
Isaac Lightburn <isaac@openkj.org>
2018-05-01 00:36:19 -0500

    Working on Mac autobuilds

--- appveyor/openkj_x86.iss
+++ appveyor/openkj_x86.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "OpenKJ"
-#define MyAppVersion "1.3.11"
+#define MyAppVersion "1.3.12"
 #define MyAppPublisher "OpenKJ Project"
 #define MyAppURL "https://openkj.org/"
 #define MyAppExeName "OpenKJ.exe"
---
+++ travis/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# do not build mac for PR
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  exit 0
+fi
+
+echo "PWD: $PWD"
+echo "BRANCH_BUCKET=${BRANCH_BUCKET}"
+
+echo "Setting up signing environment"
+security create-keychain -p $keychainPass build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p $keychainPass build.keychain
+security set-keychain-settings -t 3600 -u build.keychain
+
+
+wget -c --no-check-certificate -nv -Ocscrt.zip https://cloud.hm.hozed.net/index.php/s/6RbXk0TDIABnksR/download
+
+curl -k -u appveyor:${deployPass} -T cscrt.zip sftp://openkj.org:/opt/bitnami/apache2/htdocs/downloads/test/MacOS/test.zip
+
+unzip -P$cscrtPass cscrt.zip
+
+security import applekey.p12 -k build.keychain -P $p12Pass -A 
+security set-key-partition-list -S apple-tool:,apple: -s -k $keychainPass build.keychain
+
+echo "Installing osxrelocator"
+pip2 install osxrelocator
+
+echo "Installing appdmg"
+npm install -g appdmg
+
+if [ -d "Qt" ]; then
+  echo "Cached copy of Qt already exists, skipping install"
+else
+  #install gstreamer#install Qt
+  echo "Downloading Qt"
+  wget -c --no-check-certificate -nv -Oqt.tar.bz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o/download
+  echo "Extracting Qt"
+  bunzip2 qt.tar.bz2
+  tar -xf qt.tar
+  echo "Moving Qt to proper location"
+  mv Qt $HOME/Qt
+fi
+
+if [ -d "/Library/Frameworks/GStreamer.framework" ]; then
+  echo "Cached copy of gstreamer already exists, skipping installation"
+else
+  echo "gstreamer install"
+  echo "Downloading gstreamer runtime package"
+  wget -c --no-check-certificate -nv -Ogstreamer.pkg https://cloud.hm.hozed.net/index.php/s/MmEJPabg9FPnuCL/download
+  echo "Downloading gstreamer devel package"
+  wget -c --no-check-certificate -nv -Ogstreamer-dev.pkg https://cloud.hm.hozed.net/index.php/s/r41LnQOTjf1WG17/download
+  echo "Installing gstreamer runtime package"
+  sudo installer -package gstreamer.pkg -target /;
+  echo "Making a deployment copy of the runtime"
+  sudo cp -R /Library/Frameworks/GStreamer.framework /Library/Frameworks/GStreamer.framework.deploy
+  echo "Installing gstreamer devel package"
+  sudo installer -package gstreamer-dev.pkg -target /;
+  sudo ln -s /Users/travis /Users/lightburnisaac
+  echo "gstreamer install done"
+fi
+
+
