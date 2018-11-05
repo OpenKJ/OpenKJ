737a27c5e2ddc570a2c96d654231127e416417d0
Isaac Lightburn <isaac@openkj.org>
2018-11-04 20:08:36 -0600

    Fix price not showing properly on song shop purchase dialog

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 5
-#define OKJ_VERSION_BUILD 3
-#define OKJ_VERSION_STRING "1.5.3"
+#define OKJ_VERSION_BUILD 4
+#define OKJ_VERSION_STRING "1.5.4"
 #define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
