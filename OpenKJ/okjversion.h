7fd7ee011881821e603d57afb42503328be234c8
Isaac Lightburn <isaac@openkj.org>
2019-10-11 16:00:01 -0500

    Roll back Qt version to 5.12.4 on Windows

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 6
-#define OKJ_VERSION_BUILD 1
-#define OKJ_VERSION_STRING "1.6.1"
+#define OKJ_VERSION_BUILD 2
+#define OKJ_VERSION_STRING "1.6.2"
 #define OKJ_VERSION_BRANCH "stable"
 
 #endif //OKJVERSION_H
