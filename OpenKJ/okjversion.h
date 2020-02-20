fe2a6e6203d2ed75fbf679f85579bf456997bad1
Isaac Lightburn <lightburnisaac@psychopath.hm.hozed.net>
2020-02-19 23:25:15 -0600

    Fix program starting up with break tab selected (unintentional change)

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 7
-#define OKJ_VERSION_BUILD 29
-#define OKJ_VERSION_STRING "1.7.29"
+#define OKJ_VERSION_BUILD 30
+#define OKJ_VERSION_STRING "1.7.30"
 #define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
