4189cecad1796ef3f81e1f2d7265cf46a714245b
Isaac Lightburn <lightburnisaac@psychopath.hm.hozed.net>
2020-03-06 17:53:06 -0600

    Changing back to older xcode to see if it fixes problems on old macbooks

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 7
-#define OKJ_VERSION_BUILD 45
-#define OKJ_VERSION_STRING "1.7.45"
+#define OKJ_VERSION_BUILD 46
+#define OKJ_VERSION_STRING "1.7.46"
 #define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
