0e5c8bc34d6f159c1a972460c4e521eefc33d032
Isaac Lightburn <lightburnisaac@psychotic.hm.hozed.net>
2020-07-16 16:27:23 -0500

    Working on fixing rpm builds

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 7
-#define OKJ_VERSION_BUILD 91
-#define OKJ_VERSION_STRING "1.7.91"
+#define OKJ_VERSION_BUILD 92
+#define OKJ_VERSION_STRING "1.7.92"
 #define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
