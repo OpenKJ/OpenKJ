7fcf10bd75bad19be09acb917f612d9d6c68c0a6
Anders Thomsen <mail@andersthomsen.dk>
2021-01-30 12:09:25 +0100

    Use MediaBackend i previewer

--- OpenKJ/dlgvideopreview.h
+++ OpenKJ/dlgvideopreview.h
@@ -3,8 +3,10 @@
 
 #include <QDialog>
 #include <QTemporaryDir>
+#include <mediabackend.h>
 #include <gst/gst.h>
-#include <libCDG/include/libCDG.h>
+//#include <libCDG/include/libCDG.h>
+#include "libCDG/src/cdgfilereader.h"
 
 namespace Ui {
 class DlgVideoPreview;
@@ -20,18 +22,19 @@ public:
 
 private:
     Ui::DlgVideoPreview *ui;
-    bool m_cdgMode{false};
+    MediaBackend m_mediaBackend { this, "PREVIEW", MediaBackend::VideoPreview };
+    //bool m_cdgMode{false};
     QString m_mediaFilename;
     QTemporaryDir tmpDir;
-    GstElement *pipeline{nullptr};
-    GstElement *playBin{nullptr};
-    GstElement *videoSink{nullptr};
-    GstElement *appSrc{nullptr};
-    CdgParser parser;
+    //GstElement *pipeline{nullptr};
+    //GstElement *playBin{nullptr};
+    //GstElement *videoSink{nullptr};
+    //GstElement *appSrc{nullptr};
+    //CdgParser parser;
     guint64 position{0};
-    unsigned int curFrame{0};
-    static void cb_need_data(GstElement *appsrc, guint unused_size, gpointer user_data);
-    static gboolean cb_seek_data(GstElement *appsrc, guint64 position, gpointer user_data);
+    //unsigned int curFrame{0};
+    //static void cb_need_data(GstElement *appsrc, guint unused_size, gpointer user_data);
+    //static gboolean cb_seek_data(GstElement *appsrc, guint64 position, gpointer user_data);
     void playCdg(const QString &filename);
     void playVideo(const QString &filename);
 };
