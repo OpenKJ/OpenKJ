10a4c7e590eee594ae1f2d8c4ed95d334575e1e2
T. Isaac Lightburn <isaac@openkj.org>
2021-02-21 23:05:33 -0600

    Use std library components instead of QString for search matching for wider compatibility

--- OpenKJ/tablemodelbreaksongs.cpp
+++ OpenKJ/tablemodelbreaksongs.cpp
@@ -13,6 +13,20 @@ QDebug operator<<(QDebug debug, const BreakSong &b)
     return debug;
 }
 
+std::vector<std::string> split(const std::string& s, char seperator)
+{
+    std::vector<std::string> output;
+    std::string::size_type prev_pos = 0, pos = 0;
+    while((pos = s.find(seperator, pos)) != std::string::npos)
+    {
+        std::string substring( s.substr(prev_pos, pos-prev_pos) );
+        output.push_back(substring);
+        prev_pos = ++pos;
+    }
+    output.push_back(s.substr(prev_pos, pos-prev_pos)); // Last word
+    return output;
+}
+
 TableModelBreakSongs::TableModelBreakSongs(QObject *parent)
     : QAbstractTableModel(parent)
 {
@@ -109,14 +123,14 @@ void TableModelBreakSongs::search(const QString &searchStr)
 {
     m_lastSearch = searchStr;
     emit layoutAboutToBeChanged();
-    auto searchTerms = searchStr.split(" ", Qt::SplitBehaviorFlags::SkipEmptyParts);
+    auto searchTerms = split(searchStr.toStdString(), ' ');
     m_filteredSongs.clear();
     m_filteredSongs.resize(m_allSongs.size());
     auto it = std::copy_if(m_allSongs.begin(), m_allSongs.end(), m_filteredSongs.begin(), [&searchTerms] (BreakSong &song)
     {
-            for (auto i=0; i<searchTerms.size(); i++)
+            for (size_t i=0; i<searchTerms.size(); i++)
     {
-        if (!song.searchString.contains(searchTerms.at(i)))
+        if (song.searchString.toStdString().find(searchTerms.at(i)) == std::string::npos)
             return false;
     }
     return true;
