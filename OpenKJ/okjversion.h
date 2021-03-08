f73c717aefa5070d9a94e795c8a84f2f3e6e2ce9
T. Isaac Lightburn <isaac@openkj.org>
2021-03-08 07:18:12 -0600

    Fix cmake conditional for Windows

--- OpenKJ/okjversion.h
+++ OpenKJ/okjversion.h
@@ -3,8 +3,8 @@
 
 #define OKJ_VERSION_MAJOR 1
 #define OKJ_VERSION_MINOR 9
-#define OKJ_VERSION_BUILD 51
-#define OKJ_VERSION_STRING "1.9.51"
+#define OKJ_VERSION_BUILD 52
+#define OKJ_VERSION_STRING "1.9.52"
 #define OKJ_VERSION_BRANCH "unstable"
 
 #endif //OKJVERSION_H
